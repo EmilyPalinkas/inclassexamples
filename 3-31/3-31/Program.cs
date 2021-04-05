e6b1ac30394d7c57f9df819bf8002d88e7016448
Emily Palinkas <Emilypalinkas@ou.edu>
2021-04-05 13:24:42 -0500

    in class ex done

--- 3-31/3-31/Program.cs
+++ 3-31/3-31/Program.cs
@@ -6,7 +6,37 @@ namespace _3_31
     {
         static void Main(string[] args)
         {
-            lLiarlllL
+            List<double> gpas = new List<double>();
+            string answer;
+
+            do
+            {
+                double gpa = ValidateDoubleInput("Please enter a GPA >>");
+                gpas.Add(gpa);
+
+
+                bool should_contiue
+                Console.WriteLine("Do you want to enter more numbers? >>");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower()[0] == 'y');
+
+            static double ValidateDoubleInput(string message)
+            {
+                double value;
+                string answer;
+                do
+                {
+                    Console.WriteLine(message);
+                    answer = Console.ReadLine();
+
+
+                } while (double.TryParse(answer, out value) == false);
+
+
+
+            }
+
         }
     }
 }
---
+++ 4-5/4-5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            do
+            {
+                Console.WriteLine("Please input your grade in the course >>");
+                double grade = Convert.ToDouble(Console.ReadLine());
+
+                string lg = ConvertGradeToLetterGrade(grade);
+
+                Console.WriteLine($"Congrats you have an {lg} in the course!");
+
+                Console.WriteLine("Do you want to convert another grade? yes or no >>");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower()[0] == 'y');
+        }
+
+       
+        /// <summary>
+        /// Converts standard grade(e.g. 98.5) to the appropriate Letter Grade (e.g. A)
+        /// </summary>
+        /// <param name="anyVariableNameWeWantThisToBekongToThisMethod">The grade to convert to a letter grade</param>
+        /// <returns>A string that is the letter grade of the input. N/A if ti is below 0</returns>
+        static string ConvertGradeToLetterGrade(double anyVariableNameWeWantThisToBekongToThisMethod)
+        {
+            string letterGrade;
+            if (anyVariableNameWeWantThisToBekongToThisMethod >=90)
+            {
+                letterGrade = "A";
+            }
+            else if (anyVariableNameWeWantThisToBekongToThisMethod >= 80)
+            {
+                letterGrade = "B";
+            }
+            else if (anyVariableNameWeWantThisToBekongToThisMethod >= 70)
+            {
+                letterGrade = "C";
+            }
+            else if (anyVariableNameWeWantThisToBekongToThisMethod >= 60)
+            {
+                letterGrade = "D";
+            }
+            else if (anyVariableNameWeWantThisToBekongToThisMethod >= 0)
+            {
+                letterGrade = "F";
+            }
+            else
+            {
+                letterGrade = "N/A";
+            }
+
+            return letterGrade;
+        }
+    }
+}
